16d34576c78f5e0da01b2b31550a91eb4eb5458e
SetsuoK <57286036+SetsuoK@users.noreply.github.com>
2025-11-04 16:35:57 +0900

    Update feedback_engine.py

--- feedback_engine.py
+++ feedback_engine.py
@@ -338,10 +338,10 @@ class TemplateRepo:
                 if pair not in candidates:
                     candidates.append(pair)
 
-        print("df["metric_type"] =",df["metric_type"] )
-        print("df["risk_level"] =",df["risk_level"] )
-        print("df["age_norm"] =",df["age_norm"] )
-        print("df["sex_norm"] =",df["sex_norm"] )
+        print("df[metric_type] =",df["metric_type"] )
+        print("df[risk_level] =",df["risk_level"] )
+        print("df[age_norm] =",df["age_norm"] )
+        print("df[sex_norm] =",df["sex_norm"] )
     
         for a, s in candidates:
             q = df[
